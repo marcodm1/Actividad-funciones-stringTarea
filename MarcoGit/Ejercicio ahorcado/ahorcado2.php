1c5b152e463dcb0229462e2eb01c8e046765a949
Marco Dominguez <marcodomat@gmail.com>
2020-11-21 10:27:10 +0100

    ff

--- MarcoGit/Ejercicio ahorcado/ahorcado2.php
+++ MarcoGit/Ejercicio ahorcado/ahorcado2.php
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
+<?php
+    if (isset($_POST['frase']) ){
+        setcookie("frase", $_POST['frase'], time() +3600);  // frase para adivinar
+        setcookie("vidas", 5, time() +3600);                // cantidad de vidas
+        $convertida = convertir($_POST['frase']);           // creamos la frase convertida
+        setcookie("fraseOculta", $convertida, time() +3600);// creamos cokie de la frase
+    }
+
+    if (isset($_POST['char']) ){ 
+        setcookie("char", $_POST['char'], time() +3600);
+    } // char para adivinar
+
+    
+                   
+?>
+
 <html lang="es">
 	<head>
 		<title>Marco Domínguez</title>
@@ -7,89 +23,99 @@
         <meta name="description" content="ahorcado">
 	</head>
 	<body>
-
         <?php 
-             
-            if (isset($_POST['frase'])){
-                $frase = $_POST['frase'];
-
+            if (isset($_COOKIE['frase']) ){
+                $frase = $_COOKIE['frase'];
+                
                 $fraseOculta = convertir($frase);
                 echo $fraseOculta . "<br>";
             
+                formulario();
+
+                $fraseOculta = convertir2($frase);
+                echo $fraseOculta . "<br>";
                
-                if (isset($_POST['char'])){
-                    
-                    $letra = $_POST['char'];
-                    $fraseOcultaModificada = adivinar($letra, $frase, $fraseOculta);
-                    echo $fraseOcultaModificada;
-                }
-            }
+
+            }else echo "no se ha pasado ninguna frase";
        
 
-
             function convertir($frase){
-                $aux = null;
-                //$letraUsuario = $_POST['char'];
-                // $caracteres = array();
+                $builder = null;
+                for ($i=0; $i<strlen($frase); $i++){
+                    $posicion = $frase[$i];
+
+                    if ($posicion == " "){
+                        $builder = $builder . " ";
+                    }else{
+                        $builder = $builder . "_";
+                    }
+                }
+                return $builder;
+            }
+
+
+            function convertir2 ($frase){
+                $builder = null;
+               
+                if (isset($_POST['char'])){
+                    $letra = $_POST['char'];
+                }
 
                 for ($i=0; $i<strlen($frase); $i++){
                     $posicion = $frase[$i];
 
                     if ($posicion == " "){
-                        $aux = $aux . " ";
+                        $builder = $builder . " ";
+                    }else{
+                        if ($posicion == $letra){
+                            $builder = $builder . $letra;
+                        }else{
+                            $builder = $builder . "_";
+                        }
                     }
-
-                    if($posicion != " "){
-                        $aux = $aux . "*";
-                    }
-
-                    // if($posicion == $letraUsuario){
-                    //     $aux = $aux . $letraUsuario;
-                    // }
-
-                    // if($posicion != $letraUsuario && $posicion != " "){
-                    //     $aux = $aux . "*";
-                    // }
-                    
-                    
                 }
-                return $aux;
+                return $builder;
             }
 
 
-            function adivinar($letra, $frase, $fraseOculta){
+            // function adivinar($letra, $frase, $fraseOculta){
 
-                for ($i=0; $i<strlen($frase); $i++){
-                    if ($letra == $frase[$i]){
-                        $fraseOculta[$i] = $letra;
-                    }
-                }
-                return $fraseOculta;
-            }
+            //     for ($i=0; $i<strlen($frase); $i++){
+            //         if ($letra == $frase[$i]){
+            //             $fraseOculta[$i] = $letra;
+            //         }
+            //     }
+            //     return $fraseOculta;
+            // }
 
          
 
             //<input type="hidden" name="caracter" value=' . $aniadido = $aniadido + $_POST['char'] . '>
 
-            echo'
+            function formulario(){
+                ?>
+
                 <form action="ahorcado2.php" method="post">
                     Introduzca una letra <input type="text" name="char" id="txt" ><br><br>
-                    <input type="hidden" name="frase" value="' . $_POST['frase'] . '">
+                    <input type="hidden" name="frase" value="<?php $_POST['frase']?>">
                     <input type="submit" value="enviar">
                 </form>
+
+                <?php
+            }
+            
                 
                 
-                
-                _______<br>
-               ..|......|<br>
-               ..|......0<br>
-               ..|.....´|`<br>
-               ..|.....´ `<br>
-               _|___<br>
+            //     _______<br>
+            //    ..|......|<br>
+            //    ..|......0<br>
+            //    ..|.....´|`<br>
+            //    ..|.....´ `<br>
+            //    _|___<br>
 
 
 
-                ';
+               
 
         ?>
         
