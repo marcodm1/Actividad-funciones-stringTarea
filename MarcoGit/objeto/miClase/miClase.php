05f47c1abf04e7c9161961b20730cf64bd9c99b1
unknown <marcodomat@gmail.com>
2020-12-04 13:35:52 +0100

    ajustes del ejercicio x

--- MarcoGit/objeto/miClase/miClase.php
+++ MarcoGit/objeto/miClase/miClase.php
@@ -4,14 +4,10 @@
     class Coche implements interMotor {
         private $color;
         private $precio;
-
-
         private function __construct($color, $precio) {
             $this->color  = $color;
             $this->precio = $precio;
         }
-
-
         private function arrancar(){}
         private function frenar(){}
         private function acelerar(){}
@@ -21,14 +17,11 @@
     class Quad extends Coche {
         private $modelo;
         private $rejilla;
-
-
-        private function __construct($modelo, $rejilla, $color, $precio) {
+        private function __construct($modelo, $rejilla) {
             parent::__construct($color, $precio);
             $this->modelo  = $modelo;
             $this->rejilla = $rejilla;
         }
-
         private function caballito(){}
     }
         
@@ -36,27 +29,16 @@
     class MaquinaCoser implements interMotor {
         private $potencia;
         private $hilo;
-
-
         private function __construct($potencia, $hilo) {
             $this->potencia  = $potencia;
             $this->hilo = $hilo;
         }
-
-
         private function arrancar(){}
         private function frenar(){}
         private function acelerar(){}
 
         private function pinchar(){}
-        
-
-        
     }
-
-
-
-    
    
 ?>
        
