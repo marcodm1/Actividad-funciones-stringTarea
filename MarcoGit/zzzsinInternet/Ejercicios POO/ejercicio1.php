b3d19ad981bf7bb379d3ad839bccf6ca1ddb2e35
unknown <marcodomat@gmail.com>
2020-12-10 11:10:41 +0100

    ejercicios en clase

---
+++ MarcoGit/zzzsinInternet/Ejercicios POO/ejercicio1.php
@@ -0,0 +1,113 @@
+<?php
+    class Figura {
+        private $nombre;
+        private $color;
+        private $esRellena = false;
+
+        public function __constructor($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function estaRellena(){
+            $this->esRellena = true;
+        }
+
+        public function estaVacia(){
+            $this->esRellena = true;
+        }
+        
+        public function showInfo(){
+            echo "El nombre es: " . $nombre.getNombre();
+            echo "El color es: "  . $color.getColor();
+            if ($esRellena){
+                echo "La figura esta rellena.";
+            }else {
+                echo "La figura no esta rellena.";
+            }
+
+            // mostrar propiedades de as dos hijas
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        final function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function getColor(){
+            return $this->color;
+        }
+
+        public function setColor($color){
+            $this->color = $color;
+        }
+
+    }
+
+    class Circulo extends Figura(){
+        private $radio;
+
+        public function __constructor($radio, $nombre){
+            parent::__construct($nombre);
+            $this->radio = $radio
+        }
+
+        public getRadio(){
+            return $this->radio;
+        }
+
+        public setRadio($radio){
+            $this->radio = $radio;
+        }
+
+        public getNombre(){
+            return "circulo";
+        }
+
+        public getArea(){
+            $area = pi() * pow($radio.getRadio(), 2); 
+            return "El area del circulo es: " . $area;
+        }
+
+    }
+
+    class Cuadrado extends Figura(){
+        private $lado;
+
+        public function __constructor($lado, $nombre){
+            parent::__construct($nombre);
+            $this->lado = $lado
+        }
+
+        public getLado(){
+            return $this->lado;
+        }
+
+        public setLado($lado){
+            $this->lado = $lado;
+        }
+
+        public getArea(){
+            $area = pow($lado.getLado(), 2); 
+            return "El area del cuadrado es: " . $area;
+        }
+
+    }
+
+
+    $figura = new Figura("cuadrado");
+    $figura.setColor("azul");
+    $figura.estaRellena();
+    $figura.showInfo();
+    // _____________________
+    $cirulo = new Circulo(10, "circulo");
+    $circulo.getArea();
+    // _____________________
+    $cuadrado = new Cuadrado(5, "cuadrado");
+
+
+
+
+?>
