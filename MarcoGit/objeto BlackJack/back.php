03ec557d1b97d5fc75086bf70c9db9d98b7c998c
Marco Dominguez <marcodomat@gmail.com>
2020-12-15 16:30:14 +0100

    actualizaciones

--- MarcoGit/objeto BlackJack/back.php
+++ MarcoGit/objeto BlackJack/back.php
@@ -1,22 +1,16 @@
 
 <?php
-    // include "interface.php";
-
-    if (isset($_POST['nombre'])){
-    }else {
-        header("Location:menu.php");
-    }
 
     class Jugador {
-        private $dineroTotal;
+        private $cartera;
 
-        public function __construct($dineroTotal){
-            $this->dineroTotal = $dineroTotal;
+        public function __construct($cartera) {
+            $this->cartera = $cartera;
         }
 
         private function apostar($num) {
-            if ($num < $this->dineroTotal){
-                $this->dineroTotal = $this->dineroTotal - $num;
+            if ($num < $this->cartera) {
+                $this->cartera = $this->cartera - $num;
                 return $num;
             }else {
                 $error = "Error: No puedes apostar mas de lo que tienes total.";
@@ -25,7 +19,16 @@
             
         }
         // private function plantarse() {}
-        // private function retirarse() {}
+        // private function pedirCarta() {}
+        private function cambiarAS($num) {
+            if ($num == 1) {
+                return $this->numeroCarta = 11;
+            }else {
+                return "Error: Solo se puede modificar el 1 por el 11.";
+            }
+        
+        }
+
     }
 
     class Carta {
@@ -46,40 +49,49 @@
                     }
             }
         }
-
-        private function cambiarAS($num) {
-            if ($num == 1){
-                return $this->numeroCarta = 11;
-            }else {
-                return "Error: solo puedes modificar el 1 por el 11";
-            }
-        }
     }  
 
+    class Casino {
+        private $caja;
+
+        public function __construct($num){
+            $this->caja = $num;
+        }
+
+        // private function repartir(){
+        //     // -2 a bajara para jugador1
+        //     // -2 a naraka para casiono
+        //     $aux = 0;
+        //     for ($i=0; $i<2; $i++) {
+        //         $num = rand(1, count($this->barajaCartas));
+        //         $aux = $this->barajaCartas[$num];
+        //     }
+        // }
+
+        // private function comenzarJuego() {}
+        // private function recogerCartas() {}
+        // private function entregarDinero() {}
+        // private function recogerDinero() {}
+
+
+    }
+
     class Baraja { 
-        private const CANTIDAD = 52;
         private $barajaCartas = array();
         
-        
         public function __construct(){
-            $J  = 10;
-            $Q  = 10;
-            $K  = 10;
-            $AS = 11;
 
             for ($i=0; $i<4; $i++){
-                for ($j=0; $j<13; $j++){
-                    if ($j >= 10){
-                        $barajaCartas[] = new Carta($j);
-                    }else{
-                        $barajaCartas[] = new Carta($j);
-                    }
+                for ($j=1; $j<14; $j++){
+                    $barajaCartas[] = new Carta($j);
                 }
-                $j = 0;
+                $j = 1;
             }
         }
 
-        function repartir(){
+        // private function quitarCarta(){}
+
+        private function repartir(){
             // le quita 2 objetos a baraja para jugador1
             // le quita 2 objetos a baraja para IA
             $aux = 0;
@@ -96,20 +108,13 @@
 
     
 
-    function otraCarta(){
-        $num = rand(1, 13);
-        $carta = new Carta($num);
-        return $carta;
-    }
 
-    function terminar(){
-        return true;
-    }
+   
 
     // -------- programa ---------
     
     $baraja = new Baraja();
-    $baraja->repartir(); // 2 al jugador
+    // $baraja->repartir(); // 2 al jugador
     // repartir 2 cartas a cada uno
 
     // $respuesta = true/false 
