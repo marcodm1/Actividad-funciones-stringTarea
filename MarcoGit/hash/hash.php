646227d1c07cd6cc7e7727bc1d2aac4413be74d4
Marco <marcodomat@gmail.com>
2021-02-04 10:02:02 +0100

    dd

--- MarcoGit/hash/hash.php
+++ MarcoGit/hash/hash.php
@@ -1,20 +1,30 @@
 <?php
-class Password {
-    const SALT = 'EstoEsUnSalt';
-    public static function hash($password) {
-        return hash('sha512', self::SALT . $password);
+    class Password {
+        const SALT = 'EstoEsUnSalt';
+        public static function hash($password) {
+            return hash('sha512', self::SALT . $password);
+        }
+        public static function verify($password, $hash) {
+            return ($hash == self::hash($password));
+        }
     }
-    public static function verify($password, $hash) {
-        return ($hash == self::hash($password));
+    // Crear la contraseña:
+    $hash = Password::hash('micontraseña'); // esto lo hemos visto en clase
+    // Comprobar la contraseña introducida
+    if (Password::verify('micontraseña', $hash)) {
+        echo 'Contraseña correcta!\n';
+    } else {
+        echo "Contraseña incorrecta!\n";
     }
-}
-// Crear la contraseña:
-$hash = Password::hash('micontraseña'); // esto lo hemos visto en clase
-// Comprobar la contraseña introducida
-if (Password::verify('micontraseña', $hash)) {
-    echo 'Contraseña correcta!\n';
-} else {
-    echo "Contraseña incorrecta!\n";
-}
 
+
+
+    // mas cosas 
+    echo $hash = password_hash('micontraseña', PASSWORD_DEFAULT, [15]);
+
+    if (password_verify('micontraseña', $hash))
+    echo "correcto";
+    else echo 'incorrecto';
 ?>
+
+
