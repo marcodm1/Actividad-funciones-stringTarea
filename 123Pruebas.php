826b399d2be303db410b09c7dc3a9b9e5271a64a
Marco Dominguez <marcodomat@gmail.com>
2021-04-22 19:59:20 +0200

    123

--- 123Pruebas.php
+++ 123Pruebas.php
@@ -1,16 +1,13 @@
 <?php
-    if (!empty($_POST['nombre']) && !empty($_POST['edad']) && !empty($_POST['plato']) && !empty($_POST['horario'])) {
-        echo "<ol>";
-        foreach ($_POST as $algo) {
-            if (is_array($algo)) {
-                foreach ($algo as $clave => $valor) {
-                    echo "<li>" . $valor . "</li>";
-                }
-            }else {
-                echo "<li>" . $algo . "</li><br>";
-            }
-        }
-        echo "</ol>";
+    if (!empty($_POST['nombre']) ) {
+        $a = "nombre";
+
+        var_dump($_POST['nombre']);
+        echo "<br>";
+        var_dump($_POST[$a]);
+        
+
+
     }else {
         formulario();
     }
@@ -22,24 +19,6 @@
             <label for="txtnombre">Nombre del nuevo restaurante?:</label>
                 <input type="text" name="nombre" id="txtnombre" minlength="5" value="<?php if(isset($_POST['nombre'])){echo $_POST['nombre'];} ?>"><br><br> <!-- no me funciona minlength -->
 
-            <label for="numEdad">Es mayor de 18 años?:</label>
-                <input type="checkbox" name="edad" id="numEdad" ><br><br>
-
-            <label for="plato">Seleccione su plato principal:</label><br>
-                    <input type="radio" name="plato" value="macarrones">
-                <label for="macarrones">Macarrones con chorizo</label><br>
-                    <input type="radio" name="plato" value="carbonara">
-                <label for="carbonara"> Pasta carbonara </label><br>
-                    <input type="radio" name="plato" value="pizza">
-                <label for="pizza">     Pizza           </label><br>
-                    <input type="radio" name="plato" value="burguer">
-                <label for="burguer">   Burguer         </label><br><br>
-
-            <label for="horario">Seleccione su horario:</label>
-                <select name="horario[]">
-                    <option value="comidas">Comidas</option>
-                    <option value="cenas">Cenas</option>
-                </select><br>
             <input type="submit" value="enviar">
         </form>
         <?php
--- zzzTema5/ejercicio2.php
+++ zzzTema5/ejercicio2.php
@@ -22,34 +22,79 @@
         <form action="ejercicio2.php" method="post">
             
             <label for="txtnombre">Nombre del nuevo restaurante?:</label>
-                <input type="text" name="nombre" id="txtnombre" minlength="5" value="<?php if(isset($_POST['nombre'])){echo $_POST['nombre'];} ?>"><br><br> <!-- no me funciona minlength -->
+                <input type="text"     name="nombre"    id="txtnombre" value="<?php echo comprobar("nombre"); ?>"><br><br> <!-- no me funciona minlength -->
 
             <label for="numEdad">Es mayor de 18 años?:</label>
-                <input type="checkbox" name="edadmas" id="numEdad" value="acepta1" <?php if(isset($_POST['edadmas'])){echo 'checked="checked"';} ?> ><br><br>
+                <input type="checkbox" name="edadmas"   id="numEdad" <?php echo comprobar("edadmas"); ?>><br><br>
 
             <label for="numEdad">Es menor de 18 años?:</label>
-                <input type="checkbox" name="edadmenos" id="numEdad" value="acepta2" <?php if(isset($_POST['edadmenos'])){echo 'checked="checked"';} ?> ><br><br>
+                <input type="checkbox" name="edadmenos" id="numEdad" <?php echo comprobar("edadmenos"); ?>><br><br>
 
             <!-- probar a hacer un array de checkeds -->
-
+            <label for="checkList[]">Seleccione lo que quiere en la mesa:</label><br> <!-- no se si hay que poner o no esta linea -->
+            <input type="checkbox" name="checkList[]" value="cuchara"    <?php echo comprobarAñadidos("checkList", "cuchara") ?>>   <label>cuchara     </label><br/>
+            <input type="checkbox" name="checkList[]" value="tenedor"    <?php echo comprobarAñadidos("checkList", "tenedor") ?>>   <label>tenedor     </label><br/>
+            <input type="checkbox" name="checkList[]" value="cuchillo"   <?php echo comprobarAñadidos("checkList", "cuchillo") ?>>  <label>cuchillo    </label><br/>
+            <input type="checkbox" name="checkList[]" value="plato"      <?php echo comprobarAñadidos("checkList", "plato") ?>>     <label>plato       </label><br/>
+            <input type="checkbox" name="checkList[]" value="vaso"       <?php echo comprobarAñadidos("checkList", "vaso") ?>>      <label>vaso        </label><br/>
+            <input type="checkbox" name="checkList[]" value="copa"       <?php echo comprobarAñadidos("checkList", "copa") ?>>      <label>copa        </label><br/>
+            <input type="checkbox" name="checkList[]" value="servilleta" <?php echo comprobarAñadidos("checkList", "servilleta") ?>><label>servilleta  </label><br/>
+          
             <label for="plato">Seleccione su plato principal:</label><br>
-                    <input type="radio" name="plato" value="macarrones" <?php if(isset($_POST['plato']) && $_POST['plato'] == "macarrones"){echo 'checked="checked"';} ?> >
+                    <input type="radio" name="plato" value="macarrones" <?php echo comprobarRadio("plato", "macarrones") ?>>
                 <label for="macarrones">Macarrones con chorizo</label><br>
-                    <input type="radio" name="plato" value="carbonara" <?php if(isset($_POST['plato']) && $_POST['plato'] == "carbonara"){echo 'checked="checked"';} ?> >
+                    <input type="radio" name="plato" value="carbonara"  <?php echo comprobarRadio("plato", "carbonara") ?>>
                 <label for="carbonara"> Pasta carbonara </label><br>
-                    <input type="radio" name="plato" value="pizza" <?php if(isset($_POST['plato']) && $_POST['plato'] == "pizza"){echo 'checked="checked"';} ?> >
+                    <input type="radio" name="plato" value="pizza"      <?php echo comprobarRadio("plato", "pizza") ?>>
                 <label for="pizza">     Pizza           </label><br>
-                    <input type="radio" name="plato" value="burguer" <?php if(isset($_POST['plato']) && $_POST['plato'] == "burguer"){echo 'checked="checked"';} ?> >
+                    <input type="radio" name="plato" value="burguer"    <?php echo comprobarRadio("plato", "burguer") ?>>
                 <label for="burguer">   Burguer         </label><br><br>
 
             <label for="horario">Seleccione su horario:</label>
                 <select name="horario[]">
-                    <option value="comidas">Comidas</option>
-                    <option value="cenas" >Cenas</option>
+                    <option >Seleccione</option>
+                    <option value="comidas" <?php echo comprobarHorario("horario", "comidas"); ?>>Comidas</option>
+                    <option value="cenas"   <?php echo comprobarHorario("horario", "cenas"); ?>  >Cenas</option>
                 </select><br>
             <input type="submit" value="enviar">
         </form>
         <?php
     }
 
+    function comprobar($name1) {
+        $name = $name1;
+        if (isset($_POST[$name])) {
+            switch ($name) {
+                case "nombre":    return $_POST[$name];
+                case "edadmas":   return 'checked="checked"';
+                case "edadmenos": return 'checked="checked"';
+            }
+        } 
+    }
+
+    function comprobarAñadidos($name1, $extra) {
+        $name = $name1;
+        foreach ($_POST[$name] as $clave => $valor) {
+            if ($valor == $extra) {
+                return 'checked="checked"';
+            }
+        }
+    }
+
+    function comprobarRadio($name1, $comida) {
+        $name = $name1; 
+        if (isset($_POST[$name]) && $_POST[$name] == $comida) {
+            return 'checked="checked"';
+        } 
+    }
+
+    function comprobarHorario($horario1, $comer) {
+        $horario = $horario1;
+        if (isset($_POST[$horario]) && $_POST[$horario] == $comer) {
+            return 'selected="selected"';
+        } 
+    }
+
+
 ?>
+   
