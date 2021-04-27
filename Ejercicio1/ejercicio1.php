d6053f7411ac0aae6321b01db912ec916616dc35
Marco Dominguez <marcodomat@gmail.com>
2021-04-27 14:22:47 +0200

    123

---
+++ Ejercicio1/ejercicio1.php
@@ -0,0 +1,46 @@
+<?php
+
+    class Carta {
+        const RUTA = "./img/";  //que es donde va a localizar el fichero que representa la carta, será el directorio 'img'
+        private $directorio;    //carpeta donde está el fichero de imagen de la carta.
+        private $fichero        //fichero donde está la carta (solo el nombre del fichero no la ruta completa)
+        private $nombre;        //nombre de la carta.
+        private $puntos;        //puntos que tengo que contabilizar en la partida.
+        private $palo;          //nombre del palo 
+        private $numero;        //numero de la carta
+
+        public __construct($palo, $numero, $directorio = "") {
+            $this->puntos   = $numero;
+            $this->palo     = $palo;
+            $this->numero   = $numero;
+            $this->fichero  = crearFichero($this->palo, $this->numero);
+            $this->nombre   = crearNombre($nombre, $palo);
+
+            if (empty($directorio)) {
+                // "./img/"
+                $this->directorio = RUTA;
+            }else {
+                $this->directorio = $directorio;
+            }
+        }
+
+        function crearFichero($palo1, $numero1) {
+            $letra      = substr($palo1, 0, 1);
+            $numero     = $numero1;
+            $extension  = ".svg";
+            return $letra . $numero . $extension;
+        }
+
+        function crearNombre($nombre, $palo) {
+            return $nombre . $palo;
+        }
+
+        function getEtiquetaImg() {
+            echo '<img src="img/c1.svg" alt="corazones1"';
+            return ; 
+        }
+    }
+
+    $corazones1 = new Carta("corazones", 1, "./img/");
+
+?>
